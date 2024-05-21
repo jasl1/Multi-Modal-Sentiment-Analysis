3427110783bdc63437c6075143e2842424d4e7c2
Javad Rafiei Asl <57322461+jasl1@users.noreply.github.com>
2024-05-21 18:28:02 -0400

    Create Version Control.sh

---
+++ Version Control.sh
@@ -0,0 +1,6 @@
+# Commands for initializing Git repository, adding files, committing changes, and creating branches
+git init
+git add .
+git commit -m "Initial commit"
+git branch feature-1
+git checkout feature-1
